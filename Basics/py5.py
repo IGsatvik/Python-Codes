7223b04138155af20c6a9b23d12d241f0457a5b7
Satvik Mishra <satvikmishra.shubh@gmail.com>
2026-07-01 09:41:04 +0530

    Python calculator

---
+++ Basics/py5.py
@@ -0,0 +1,19 @@
+print("Welcome to Python calculator")
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+choice=int(input("Enter your choice : "))
+x=float(input("Enter first variable : "))
+y=float(input("Enter second variable : "))
+if choice == 1 :
+    print(f"Your answer is {x+y}")
+elif choice == 2 :
+    print(f"Your answer is {x-y}")
+elif choice == 3 :
+    print(f"Your answer is {x*y}")
+elif choice == 4 :
+    if y != 0 :
+        print(f"Your answer is {round(x/y,3)}")
+    else :
+        print("Division not possible")
