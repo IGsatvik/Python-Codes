b7916acc91e0ddfe9fb11c45a01c8f73864e5195
Satvik Mishra <satvikmishra.shubh@gmail.com>
2026-07-01 09:57:53 +0530

    Python Calculator
    Updated the calculator to use string inputs for operations.

--- Basics/py5.py
+++ Basics/py5.py
@@ -3,21 +3,21 @@ print("1.Addition")
 print("2.Subtraction")
 print("3.Multiplication")
 print("4.Division")
-choice=int(input("Enter your choice : "))
+choice=input("Enter your choice : ")
 x=float(input("Enter first variable : "))
 y=float(input("Enter second variable : "))
-if choice == 1 :
+if choice == '+' :
     print(f"Your answer is {x+y}")
-elif choice == 2 :
+elif choice == '-' :
     print(f"Your answer is {x-y}")
-elif choice == 3 :
+elif choice == '*' :
     print(f"Your answer is {x*y}")
-elif choice == 4 :
+elif choice == '/' :
     if y != 0 :
         print(f"Your answer is {round(x/y,3)}")
     else :
         print("Division not possible")
+        
 #use pass to skip code block
-
 if choice !=0 :
     pass
